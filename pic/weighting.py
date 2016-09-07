6010defd74fa71c1fdc3cd6259ef8876bcb244db
Ondrej Grover <ondrej.grover@gmail.com>
2016-09-07 19:21:32 +0200

    mae sure we start at 0, but later this could be done elsewhere

--- pic/weighting.py
+++ pic/weighting.py
@@ -13,6 +13,7 @@ def find_particle_in_grid(particle_pos, h, S_h):
 def cic_charge_weighting(particle_pos, particle_charge, active_particles, rho, h):
     """Cloud-in-cell charge weighting of particles to rho grid"""
     S_h = h**2                             # cell surface
+    rho.fill(0.0)             # TODO optimization: perform in some for loop
     for i in range(active_particles):
         j, k, x, y = find_particle_in_grid(particle_pos[i], h)
         rho[j,k] += partcle_charge * (h-x)*(h-y)/S_h
@@ -27,6 +28,7 @@ def cic_field_weighting(particle_pos, particle_field, active_particles, field, h
     S_h = h**2                             # cell surface
     for i in range(active_particles):
         j, k, x, y = find_particle_in_grid(particle_pos[i], h)
+        particle_field[i] = 0.0
         particle_field[i] += field[j,k] * (h-x)*(h-y)/S_h
         particle_field[i] += field[j+1,k] * x*(h-y)/S_h
         particle_field[i] += field[j+1,k+1] * x*y/S_h
