b1521fe120d35a978a7eb63c7794a0885e0844ff
Ondrej Grover <ondrej.grover@gmail.com>
2016-09-11 11:13:56 +0200

    divide force by mass to get acceleration

--- pic/movers.py
+++ pic/movers.py
@@ -3,10 +3,10 @@ import numba
 # TODO: magnetic field moving
 # TODO OPTIMIZE: merge CIC filed weighting with field usage
 @numba.jit(nopython=True)
-def electrostatic_mover(particle_pos, particle_vel, particle_charge, particle_E, active_particles, dt):
+def electrostatic_mover(particle_pos, particle_vel, particle_charge, particle_mass, particle_E, active_particles, dt):
     for i in range(active_particles):
         # velocity is 3D, E and x is 2D
-        particle_vel[i][:-1] += particle_charge[i] * particle_E[i][:] * dt
+        particle_vel[i][:-1] += particle_charge / particle_mass * particle_E[i][:] * dt
         particle_pos[i][:] += particle_vel[i][:-1] * dt
 
 
