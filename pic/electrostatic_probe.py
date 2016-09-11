2c34b58f01fba1dc8b764453adf1fe154eae3074
Ondrej Grover <ondrej.grover@gmail.com>
2016-09-11 20:16:50 +0200

    calculate and add probe potential
    for now just sets U_probe =1 in probe region and repeats several iterations
    ugly but seems to work

---
+++ pic/electrostatic_probe.py
@@ -0,0 +1,39 @@
+import numpy as np
+from pic.poisson_sor_solver import solve_poisson
+
+
+class ProbeSetup(object):
+
+    def __init__(self, grid_size, probe_size, h):
+        self.h = h
+        grid_length = grid_size * h
+        grid_center = grid_length / 2.0
+        self.probe_length = probe_size * h
+        half_width = self.probe_length / 2.0
+        self.S = self.probe_length**2
+        self.min = grid_center - half_width
+        self.max = grid_center + half_width
+        # pre-calculate potential
+        self.grid_shape = (grid_size, grid_size)
+        self.phi_1 = np.zeros(self.grid_shape)
+        # make sure the potential is given within the probe
+        i_min = int(np.ceil(self.min / h))
+        i_max = int(np.floor(self.max / h))
+        # solve the phi potential
+        rho = np.zeros(self.grid_shape)
+        for i in range(1000):
+            self.phi_1[i_min:i_max+1,i_min:i_max+1] = 1.0 # set U_probe
+            solve_poisson(rho, self.phi_1, np.nan, 0, 1)  # one iteration
+
+    def get_potential(self, U_probe):
+        return self.phi_1 * U_probe
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    probe = ProbeSetup(100, 9, 1e-10)
+    phi = probe.get_potential(1)
+    plt.matshow(phi)
+    plt.colorbar()
+    plt.show()
+
--- pic/simulation_run.py
+++ pic/simulation_run.py
@@ -3,6 +3,7 @@ from scipy.stats import norm, uniform
 import scipy.constants as spc
 import numpy as np
 from collections import namedtuple
+from pic.electrostatic_probe import ProbeSetup
 from pic.boundary_crossing import boundary_crossings, boundary_reflections
 from pic.weighting import cic_charge_weighting, cic_field_weighting
 from pic.poisson_lu_solver import create_poisson_solver
@@ -28,12 +29,13 @@ Particles = namedtuple('Particles', ('x', 'v', 'n', 'q', 'm'))
 
 
 # TODO loop over V_fl with alocated arrays, just change initial values?
-def simulate_probe_current(grid_length, N, h, T_e, dt, max_iterations, callback):
+def simulate_probe_current(probe_setup, U_probe, N, T_e, dt, max_iterations, callback):
     # TODO physical basis for parameters
-    grid_shape = (grid_length, grid_length)              # 2D grid
+    grid_shape = probe_setup.grid_shape # 2D rectangular grid
+    h = probe_setup.h
     frac_N = N // 2             # each species is fraction of N all particle
     active_particles = int(frac_N*0.75) # leave some space for extra particles
-    region_length = grid_length * h
+    region_length = grid_shape[0] * h
     # initialize particle kinematics for each species and main/reservoir regions
     regions = Regions._make(
         [Particles._make(initialize_particle_kinematics(region_length, T_e, m, frac_N)
@@ -45,12 +47,9 @@ def simulate_probe_current(grid_length, N, h, T_e, dt, max_iterations, callback)
     reservoir_ion_pos, reservoir_ion_vel = initialize_particle_kinematics(region_length, T_e, spc.m_e, frac_N)
     el_pos, el_vel = initialize_particle_kinematics(region_length, T_e, spc.m_p, frac_N)
     reservoir_el_pos, reservoir_el_vel = initialize_particle_kinematics(region_length, T_e, spc.m_e, frac_N)
-    poisson_solver = create_poisson_solver(grid_length)
-    # calculate probe position
-    probe_length = 9*h
-    grid_center = grid_length / 2.0 * h
-    probe_min = grid_center - probe_length / 2.0
-    probe_max = grid_center + probe_length / 2.0
+    # probe potential
+    phi_probe = probe_setup.get_potential(U_probe)
+    poisson_solver = create_poisson_solver(grid_shape[0])
     # initial iteration values
     iterations = 0
     rho = np.empty(grid_shape)
@@ -60,10 +59,10 @@ def simulate_probe_current(grid_length, N, h, T_e, dt, max_iterations, callback)
         lost_charge = 0.0
         for particles in regions.main: # TODO possible optimization: weight particles here
             particles.n[0], lost_in_probe = boundary_crossings(
-                particles.x, particles.v, 0, region_length, probe_min, probe_max,
+                particles.x, particles.v, 0, region_length, probe_setup.min, probe_setup.max,
                 particles.n[0])
             lost_charge += particles.q * lost_in_probe
-        probe_current = lost_charge / dt
+        j_probe = lost_charge / dt / probe.S # current density
         for s_i, particles in enumerate(regions.reservoir):
             main_particles = regions.main[s_i]
             reflected_particles = boundary_reflections(particles.x, particles.v, 0, region_length)
@@ -83,6 +82,8 @@ def simulate_probe_current(grid_length, N, h, T_e, dt, max_iterations, callback)
             cic_charge_weighting(particles.x, particles.q, particles.n[0], rho, h)
         # solve Poisson equation
         phi = poisson_solver(rho*h**2/spc.epsilon_0*1e3)
+        # add probe potential
+        phi += phi_probe
         # calculate E from phi
         E = -np.dstack(np.gradient(phi, h, h)) # TODO *-1 in some for loop
         # move particles
@@ -95,8 +96,8 @@ def simulate_probe_current(grid_length, N, h, T_e, dt, max_iterations, callback)
                                 particles.m, particle_E, particles.n[0], dt)
         # next iteration
         iterations += 1
-        callback(regions, rho, phi, iterations, probe_current)
-    return probe_current
+        callback(regions, rho, phi, iterations, j_probe)
+    return j_probe
 
 
 if __name__ == '__main__':
@@ -104,8 +105,9 @@ if __name__ == '__main__':
     plt.ion()
     def callback(reg, rho, phi, it, i):
         plt.cla()
-        plt.imshow(rho)
+        plt.imshow(phi)
         plt.pause(1.0/30)
         print('iteration', it, 'with current', i)
-    i_probe = simulate_probe_current(100, 10000, 1e-5, 50, 1e-10, 100, callback)
+    probe = ProbeSetup(100, 9, 1e-5)
+    i_probe = simulate_probe_current(probe, 1e-12, 10000, 50, 1e-10, 100, callback)
     print(i_probe)
